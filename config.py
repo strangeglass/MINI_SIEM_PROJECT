6af71aa85e7df728a722d7eaba81ae7a1df8fb99
strangeglass <kkentaroken63@gmail.com>
2026-03-01 16:32:15 +0545

    Step 16: Adding config file to define security thresholds

---
+++ config.py
@@ -0,0 +1,7 @@
+# Configuration settings for the Mini SIEM
+
+# Threshold: Number of failures to trigger an alert
+FAILURE_THRESHOLD = 5
+
+# Time Window: Number of seconds to look back for failures
+TIME_WINDOW = 60 # seconds
